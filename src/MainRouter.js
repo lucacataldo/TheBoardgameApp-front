6e8e7415dc48b1d8e0b899f747445f77a9cfe48e
Zenna Liang <zenna.liang@gmail.com>
2020-02-01 18:19:48 -0500

    signin and signup

---
+++ src/MainRouter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import Home from './components/Home';
+import Signup from './user/Signup';
+import Signin from "./user/Signin";
+
+const MainRouter = () => (
+    <div>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/signup" component={ Signup } />
+            <Route exact path="/signin" component={Signin} />
+        </Switch>
+    </div>
+);
+
+export default MainRouter;
