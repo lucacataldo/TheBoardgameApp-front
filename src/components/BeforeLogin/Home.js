47993867a6e371c545f5a41f13b8c216b6f4d0f1
Zenna Liang <zenna.liang@gmail.com>
2020-02-27 20:44:54 -0500

    commit

---
+++ src/components/BeforeLogin/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import BgLogo from "../../images/BgLogo.png";
+import NavBar from "../NavBar";
+
+class Home extends React.Component {
+  render() {
+ // TODO: redirect to posts if authenticated / login
+    return (
+      <>
+        <div className="bgImage mx-auto my-auto d-flex justify-content-center">
+       
+        <div className="mx-auto my-auto text-center col-6">
+            <img
+              className="align-items-center justify-content-center"
+              src={`${BgLogo}`}
+              alt="Boardgame Guru Logo"
+            />
+            <h2 style={{ color: "white" }}> The Boardgame Guru</h2>
+            <p style={{ color: "white" }}>
+              This all-in-one boardgame website is under development. Find out
+              more once it's done!'
+            </p>
+          </div>
+        
+        </div>
+      </>
+    );
+  }
+}
+export default Home;
