0488b828daaebc67f43d7614fd3f2406676d08b5
Zenna Liang <zenna.liang@gmail.com>
2020-03-02 15:50:15 -0500

    updated create post UI on posts page

--- src/post/NewPost.js
+++ src/post/NewPost.js
@@ -5,167 +5,163 @@ import { isAuthenticated } from "../auth";
 import { createPost } from "./apiPost";
 
 class NewPost extends Component {
-    constructor() {
-        super();
-        this.state = {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      body: "",
+      photo: "",
+      error: "",
+      user: {},
+      fileSize: 0,
+      loading: false,
+      redirectToProfile: false
+    };
+  }
+
+  componentDidMount() {
+    this.postData = new FormData();
+    this.setState({ user: isAuthenticated().user });
+  }
+
+  isValid = () => {
+    const { title, body, fileSize } = this.state;
+    if (fileSize > 100000) {
+      this.setState({
+        error: "File size should be less than 100kb",
+        loading: false
+      });
+      return false;
+    }
+    if (title.length === 0 || body.length === 0) {
+      this.setState({ error: "All fields are required", loading: false });
+      return false;
+    }
+    if (title.length > 60) {
+      this.setState({
+        error: "Title is limited to 60 characters",
+        loading: false
+      });
+      return false;
+    }
+    return true;
+  };
+
+  handleChange = name => event => {
+    this.setState({ error: "" });
+    const value = name === "photo" ? event.target.files[0] : event.target.value;
+
+    const fileSize = name === "photo" ? event.target.files[0].size : 0;
+    this.postData.set(name, value);
+    this.setState({ [name]: value, fileSize });
+  };
+
+  clickSubmit = event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+
+    if (this.isValid()) {
+      const userId = isAuthenticated().user._id;
+      const token = isAuthenticated().token;
+
+      createPost(userId, token, this.postData).then(data => {
+        if (data.error) this.setState({ error: data.error });
+        else {
+          this.setState({
+            loading: false,
             title: "",
             body: "",
-            photo: "",
-            error: "",
-            user: {},
-            fileSize: 0,
-            loading: false,
-            redirectToProfile: false
-        };
+            redirectToProfile: true
+          });
+        }
+      });
+    }
+  };
+
+  newPostForm = (title, body) => (
+    <form>
+      <label className="text-muted">Title</label>
+      <br />
+      <input
+        onChange={this.handleChange("title")}
+        type="text"
+        className="form-control"
+        value={title}
+      />
+
+      <div className="form-group mt-2">
+        <label className="text-muted">Body</label>
+        <textarea
+          rows="4"
+          onChange={this.handleChange("body")}
+          type="text"
+          className="form-control"
+          value={body}
+          style={{ resize: "none" }}
+        />
+      </div>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroupFileAddon01">
+            Upload
+          </span>
+        </div>
+        <div className="custom-file">
+          <input
+            type="file"
+            accept="image/*"
+            data-max-file-size="1M"
+            className="custom-file-input"
+            id="inputGroupFile01"
+            onChange={this.handleChange("photo")}
+            aria-describedby="inputGroupFileAddon01"
+          />
+          <label className="custom-file-label" htmlFor="inputGroupFile01">
+            Choose file
+          </label>
+        </div>
+      </div>
+
+      <button
+        onClick={this.clickSubmit}
+        className="btn btn-raised btn-primary mt-3"
+      >
+        Create Post
+      </button>
+    </form>
+  );
+
+  render() {
+    const { title, body, user, error, loading, redirectToProfile } = this.state;
+
+    if (redirectToProfile) {
+      return <Redirect to={`/user/${user._id}`} />;
     }
 
-    componentDidMount() {
-        this.postData = new FormData();
-        this.setState({ user: isAuthenticated().user });
-    }
+    return (
+      <div className="container">
+        <h1 className="mt-5 mb-4">
+          <b>Create a new post</b>
+        </h1>
+        <div
+          className="alert alert-danger"
+          style={{ display: error ? "" : "none" }}
+        >
+          {error}
+        </div>
 
-    isValid = () => {
-        const { title, body, fileSize } = this.state;
-        if (fileSize > 100000) {
-            this.setState({
-                error: "File size should be less than 100kb",
-                loading: false
-            });
-            return false;
-        }
-        if (title.length === 0 || body.length === 0) {
-            this.setState({ error: "All fields are required", loading: false });
-            return false;
-        }
-        if (title.length > 60 ) {
-            this.setState({ error: "Title is limited to 60 characters", loading: false });
-            return false;
-        }
-        return true;
-    };
+        {loading ? (
+          <div className="jumbotron text-center">
+            <h2>Loading...</h2>
+          </div>
+        ) : (
+          ""
+        )}
 
-    handleChange = name => event => {
-        this.setState({ error: "" });
-        const value =
-            name === "photo" ? event.target.files[0] : event.target.value;
-
-        const fileSize = name === "photo" ? event.target.files[0].size : 0;
-        this.postData.set(name, value);
-        this.setState({ [name]: value, fileSize });
-    };
-
-    clickSubmit = event => {
-        event.preventDefault();
-        this.setState({ loading: true });
-
-        if (this.isValid()) {
-            const userId = isAuthenticated().user._id;
-            const token = isAuthenticated().token;
-
-            createPost(userId, token, this.postData).then(data => {
-                if (data.error) this.setState({ error: data.error });
-                else {
-                    this.setState({
-                        loading: false,
-                        title: "",
-                        body: "",
-                        redirectToProfile: true
-                    });
-                }
-            });
-        }
-    };
-
-    newPostForm = (title, body) => (
-        <form>
-           
-                <label className="text-muted">Title</label><br/>
-                <input
-                    onChange={this.handleChange("title")}
-                    type="text"
-                    className="form-control"
-                    value={title}
-                />
-         
-
-            <div className="form-group mt-2">
-                <label className="text-muted">Body</label>
-                <textarea
-                    rows="4"
-                    onChange={this.handleChange("body")}
-                    type="text"
-                    className="form-control"
-                    value={body}
-                    style={{resize:"none"}}
-                />
-            </div>
-            <div className="input-group">
-  <div className="input-group-prepend">
-    <span className="input-group-text" id="inputGroupFileAddon01">
-      Upload
-    </span>
-  </div>
-  <div className="custom-file">
-    <input
-      type="file"
-      accept="image/*"
-      data-max-file-size="4M" 
-      className="custom-file-input"
-      id="inputGroupFile01"
-      onChange={this.handleChange("photo")}
-      aria-describedby="inputGroupFileAddon01"
-    />
-    <label className="custom-file-label" htmlFor="inputGroupFile01">
-      Choose file
-    </label>
-  </div>
-</div>
-        
-            <button
-                onClick={this.clickSubmit}
-                className="btn btn-raised btn-primary mt-3"
-            >
-                Create Post
-            </button>
-        </form>
+        {this.newPostForm(title, body)}
+      </div>
     );
-
-    render() {
-        const {
-            title,
-            body,
-            user,
-            error,
-            loading,
-            redirectToProfile
-        } = this.state;
-
-        if (redirectToProfile) {
-            return <Redirect to={`/user/${user._id}`} />;
-        }
-
-        return (
-            <div className="container">
-                <h1 className="mt-5 mb-4"><b>Create a new post</b></h1>
-                <div
-                    className="alert alert-danger"
-                    style={{ display: error ? "" : "none" }}
-                >
-                    {error}
-                </div>
-
-                {loading ? (
-                    <div className="jumbotron text-center">
-                        <h2>Loading...</h2>
-                    </div>
-                ) : (
-                    ""
-                )}
-
-                {this.newPostForm(title, body)}
-            </div>
-        );
-    }
+  }
 }
 
 export default NewPost;
--- src/post/Posts.js
+++ src/post/Posts.js
@@ -1,129 +1,133 @@
-import { Link, useHistory, Route } from "react-router-dom";
-import React, { useState, useRef, useCallback } from 'react';
+import { Link, Route } from "react-router-dom";
+import React, { useState, useRef, useCallback } from "react";
 
-import useFetchMorePosts from './useFetchMorePosts';
+import useFetchMorePosts from "./useFetchMorePosts";
 import DefaultPostImg from "../images/defaultPostImg.jpg";
-import { render } from "@testing-library/react";
-
-
 
 const Posts = () => {
-    const [pageNumber, setPageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
+  const { posts, hasMore, loading, error } = useFetchMorePosts(pageNumber);
+  const refObserver = useRef();
 
-    const {
-        posts,
-        hasMore,
-        loading,
-        error
-    } = useFetchMorePosts(pageNumber);
+  // whenever the <div ref={lastPostElementRef} key={i}> is created,
+  // it will call this fxn when it's the last element
+  const lastPostElementRef = useCallback(
+    node => {
+      if (loading) return;
+      // disconnect previous ref so we can reset it
+      if (refObserver.current) refObserver.current.disconnect();
+      // take all entries available
+      refObserver.current = new IntersectionObserver(entries => {
+        // if the entry is on the page and there's more to load
+        // wont keep calling api if the api provided all the items
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber(prevPageNumber => prevPageNumber + 1);
+        }
+      });
+      // observe the node if its last
+      if (node) refObserver.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
-
-    const refObserver = useRef();
-
-    // whenever the <div ref={lastPostElementRef} key={i}> is created, 
-    // it will call this fxn when it's the last element
-    const lastPostElementRef = useCallback(node => {
-        if (loading) return;
-        // disconnect previous ref so we can reset it
-        if (refObserver.current) refObserver.current.disconnect();
-        // take all entries available 
-        refObserver.current = new IntersectionObserver(entries => {
-            // if the entry is on the page and there's more to load
-            // wont keep calling api if the api provided all the items 
-            if (entries[0].isIntersecting && hasMore) {
-                setPageNumber(prevPageNumber => prevPageNumber + 1);
-            }
-        })
-        // observe the node if its last
-        if (node) refObserver.current.observe(node);
-
-    }, [loading, hasMore])
-
-    function renderPost(post) {
-        const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
-        const posterName = post.postedBy ? post.postedBy.name : " Unknown";
-            
-        return (
-            <div className="col-md-6">
-                <div className="card" >
-                    <img src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
-                        alt={post.title}
-                        onError={i => (i.target.src = `${DefaultPostImg}`)}
-                        height="200px"
-                        className="img-thumbnail card-img-top "
-                    />
-                    <div className="card-body">
-                        <h5 className="card-title">{post.title}</h5>
-                        <p className="card-text">{post.body.substring(0, 100)}</p>
-                    </div>
-                    <div className="card-footer text-muted">
-                        <div className="row justify-content-between">
-                            <div className="col-9">
-                                Posted by{" "}<Link to={`${posterId}`}>{posterName}{" "}</Link>on {new Date(post.createdDate).toDateString()}
-                            </div>
-                            <div className="col-3 text-right">
-                                <Link to={`/post/${post._id}`} className="btn btn-raised btn-primary btn-sm" >Read more</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  function renderPost(post) {
+    const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
+    const posterName = post.postedBy ? post.postedBy.name : " Unknown";
 
     return (
-        <>             
-    {/* The select Bar     */}
-    <div className="container-fluid">
-        <div className="row">
-            <div className="offset-3 col-md-6 mt-2">
-            <Route render={({ history}) => (
-                <form>
-                    <input 
-                    className="form-control"
-                    placeholder="Post some Boardgames! :3"
-                    onFocus={()=>{
-                        history.push('/post/create')
-                                }}
-                       
-                    />
-                </form>
-            )}/>
-            </div>
+      <div className="card">
+        <img
+          src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
+          alt={post.title}
+          onError={i => (i.target.src = `${DefaultPostImg}`)}
+          className="img-thumbnail postsImg mx-auto d-block"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{post.title}</h5>
+          <p className="card-text text-truncate">{post.body}</p>
         </div>
-    </div>
-            {posts.map((post, i) => {
-                if (posts.length === i + 1) {
-                    return (
-
-                        <div className="row justify-content-md-center postRow" ref={lastPostElementRef} key={i}>
-                            {renderPost(post)}
-
-                        </div>
-
-                    );
-                } else {
-                    return (
-                        <div className="row justify-content-md-center postRow" key={i}>
-                            {renderPost(post)}
-                        </div>
-                    );
-                }
-            })}
-            {!hasMore &&
-                <div className="row justify-content-md-center postRow" >
-                    No More Post
-                </div>
-            }
-            <div className="row justify-content-md-center postRow" >
-                {loading && 'Loading...'}
+        <div className="card-footer text-muted">
+          <div className="row justify-content-between">
+            <div className="col-9">
+              Posted by <Link to={`${posterId}`}>{posterName} </Link>on{" "}
+              {new Date(post.createdDate).toDateString()}
             </div>
-            <div className="row justify-content-md-center postRow" >
-                {error && 'Error'}
+            <div className="col-3 text-right">
+              <Link
+                to={`/post/${post._id}`}
+                className="btn btn-raised btn-primary btn-sm"
+              >
+                Read more
+              </Link>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    );
+  }
 
+  return (
+    <>
+      <div className="row justify-content-center postRow">
+        <div className="card">
+          <h5 className="card-header">Create Post</h5>
+          <div class="card-body">
+            <Route
+              render={({ history }) => (
+                <form>
+                  <div className="input-group">
+                    <input
+                      className="form-control"
+                      placeholder="Post some Boardgames! :3"
+                      onFocus={() => {
+                        history.push("/post/create");
+                      }}
+                    />
+                    <div className="input-group-append">
+                      <span className="input-group-text">
+                        <Link to={`/post/create`}>
+                          <i className="fa fa-image text-dark"></i>
+                        </Link>
+                      </span>
+                    </div>
+                  </div>
+                </form>
+              )}
+            />
+          </div>
+        </div>
+      </div>
+
+      {posts.map((post, i) => {
+        if (posts.length === i + 1) {
+          return (
+            <div
+              className="row justify-content-center postRow"
+              ref={lastPostElementRef}
+              key={i}
+            >
+              {renderPost(post)}
+            </div>
+          );
+        } else {
+          return (
+            <div className="row justify-content-center postRow" key={i}>
+              {renderPost(post)}
+            </div>
+          );
+        }
+      })}
+      {!hasMore && (
+        <div className="row justify-content-center postRow">No More Post</div>
+      )}
+      <div className="row justify-content-center postRow">
+        {loading && "Loading..."}
+      </div>
+      <div className="row justify-content-center postRow">
+        {error && "Error"}
+      </div>
+    </>
+  );
 };
 
 export default Posts;
