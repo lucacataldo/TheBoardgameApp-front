365c7e3543e9d779cb13858777d1c33a6e144400
Zenna Liang <zenna.liang@gmail.com>
2020-03-05 16:01:51 -0500

    updated navbar style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,120 +1,136 @@
 import React from "react";
-import { NavLink, Link, withRouter } from "react-router-dom"; // withRouter to access history location-URL link
+import { NavLink, withRouter } from "react-router-dom"; // withRouter to access history location-URL link
 import { signout, isAuthenticated } from "../auth";
 import BgLogo from "../images/BgLogo.png";
 
 class NavBar extends React.Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg  navbar-expand-md navbar-dark bg-dark">
-          <div className="container-fluid ">
-            <div className="navbar-header">
-              <Link className="navbar-brand" to="/">
-                <img src={BgLogo} width="20" height="20" alt="" /> Boardgame
-                Guru
-              </Link>
-              <button
-                className="navbar-toggler "
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-            </div>
+      <nav className="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
+        <div className="bgNavbarContainer container-fluid">
+          <NavLink className="navbar-brand" to="/posts">
+            <img src={BgLogo} width="20" height="20" alt="" /> Boardgame Guru
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#bgNavBar"
+            aria-controls="bgNavBar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="bgNavBar">
             {isAuthenticated() && (
-              <>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav mr-auto">
-                    <li className="nav-item ">
-                      <NavLink
-                        className="nav-link"
-                        activeClassName="selected"
-                        to={`/user/${isAuthenticated().user._id}`}
-                      >{`${isAuthenticated().user.name}'s Profile`}</NavLink>
-                    </li>
-                    <li className="nav-item ">
-                      <NavLink
-                        className="nav-link"
-                        activeClassName="selected"
-                        to="/posts"
-                      >
-                        Posts
-                      </NavLink>
-                    </li>
-                    <li className="nav-item ">
-                      <NavLink
-                        className="nav-link"
-                        activeClassName="selected"
-                        to="/users"
-                      >
-                        Users
-                      </NavLink>
-                    </li>
-                  </ul>
-                </div>
-              </>
-            )}
-            <div className="nav navbar-right navbar-expand-md">
               <ul className="navbar-nav mr-auto">
-                {isAuthenticated() && isAuthenticated().user.role === "admin" && (
-                  <li className="nav-item mr-auto">
+                <li className="nav-item ">
+                  <NavLink
+                    className="nav-link"
+                    activeClassName="selected"
+                    to={`/user/${isAuthenticated().user._id}`}
+                  >{`${isAuthenticated().user.name}'s Profile`}</NavLink>
+                </li>
+                <li className="nav-item ">
+                  <NavLink
+                    className="nav-link"
+                    activeClassName="selected"
+                    to="/posts"
+                  >
+                    Posts
+                  </NavLink>
+                </li>
+                <li className="nav-item ">
+                  <NavLink
+                    className="nav-link"
+                    activeClassName="selected"
+                    to="/users"
+                  >
+                    Users
+                  </NavLink>
+                </li>
+              </ul>
+            )}
+
+            <ul className="navbar-nav ">
+              {!isAuthenticated() && (
+                <>
+                  <li className="nav-item">
                     <NavLink
                       className="nav-link"
                       activeClassName="selected"
-                      to="/admin"
+                      to="/signin"
                     >
-                      Admin
+                      Sign In
                     </NavLink>
                   </li>
-                )}
-                {!isAuthenticated() && (
-                  <>
-                    <li className="nav-item mr-auto">
-                      <NavLink
-                        className="nav-link"
-                        activeClassName="selected"
-                        to="/signin"
-                      >
-                        Sign In
-                      </NavLink>
-                    </li>
-                    <li className="nav-item mr-auto">
-                      <NavLink
-                        className="nav-link d-none d-sm-block"
-                        activeClassName="selected"
-                        to="/signup"
-                      >
-                        Sign Up
-                      </NavLink>
-                    </li>
-                  </>
-                )}
-                {isAuthenticated() && (
-                  <li className="nav-item mr-auto">
-                    <span
-                      data-toggle="tooltip"
-                      title="Sign Out"
-                      aria-label="Sign Out"
-                      className="nav-link "
-                      onClick={() =>
-                        signout(() => this.props.history.push("/"))
-                      }
-                      style={{ cursor: "pointer", color: "#fff" }}
+                  <li className="nav-item ">
+                    <NavLink
+                      className="nav-link"
+                      activeClassName="selected"
+                      to="/signup"
                     >
-                      <i className="fa fa-sign-out fa-lg"></i>
-                    </span>
+                      Sign Up
+                    </NavLink>
                   </li>
-                )}
-              </ul>
-            </div>
+                </>
+              )}
+              {isAuthenticated() && (
+                <>
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link"
+                      href="/#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      <div className=" profileInitalCircle text-center">
+                        {`${isAuthenticated().user.name.substring(0, 1)}`}
+                      </div>
+                    </a>
+                    <div
+                      className="dropdown-menu"
+                      aria-labelledby="navbarDropdown"
+                    >
+                    
+                      {isAuthenticated() &&
+                        isAuthenticated().user.role === "admin" && (
+                          <>
+                            <div className="dropdown-divider"></div>
+                            <NavLink
+                              className="dropdown-item"
+                              activeClassName="selected"
+                              to="/admin"
+                            >
+                              Admin
+                            </NavLink>
+                          </>
+                        )}
+                      <div className="dropdown-divider"></div>
+                      <span
+                        className="dropdown-item"
+                        data-toggle="tooltip"
+                        title="Sign Out"
+                        aria-label="Sign Out"
+                        onClick={() =>
+                          signout(() => this.props.history.push("/"))
+                        }
+                        style={{ cursor: "pointer" }}
+                      >
+                        Sign Out
+                      </span>
+                    </div>
+                  </li>
+                </>
+              )}
+            </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     );
   }
 }
