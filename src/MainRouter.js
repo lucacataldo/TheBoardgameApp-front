1dad7cb058700d10599ab6093de097a47ff54808
Zenna Liang <zenna.liang@gmail.com>
2020-03-13 22:09:24 -0400

    added NotFound page

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,13 +3,14 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-
+import NotFound from "./components/NotFound";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import ForgotPassword from "./user/ForgotPassword";
 import ResetPassword from "./user/ResetPassword";
 
-import EditProfile from "./user/EditProfile";
+import SettingUser from "./user/SettingUser";
+import SettingCollection from "./user/SettingCollection";
 import Profile from "./user/Profile";
 import FindPeople from "./user/FindPeople";
 
@@ -17,6 +18,8 @@ import Post from "./post/Post";
 import Posts from "./post/Posts";
 import EditPost from "./post/EditPost";
 import NewPost from "./post/NewPost";
+
+import Boardgames from './boardgame/Boardgames'
 
 import Users from "./user/Users";
 import PrivateRoute from "./auth/PrivateRoute"; // only authenticated user can use
@@ -31,6 +34,7 @@ class MainRouter extends React.Component {
         <Switch>
           <Route exact path="/" render={() => <Home />} />
           <Route exact path="/posts" component={Posts} />
+          <Route exact path="/boardgames" component={Boardgames} />
           <PrivateRoute exact path="/admin" component={Admin} />
 
           <Route exact path="/forgot-password" component={ForgotPassword} />
@@ -41,6 +45,7 @@ class MainRouter extends React.Component {
           />
 
           <PrivateRoute exact path="/post/create" component={NewPost} />
+          <PrivateRoute exact path="/post/edit/:postId" component={EditPost} />
           <Route exact path="/post/:postId" component={Post} />
 
           <Route exact path="/Users" component={Users} />
@@ -52,9 +57,16 @@ class MainRouter extends React.Component {
           <PrivateRoute
             exact
             path="/user/edit/:userId"
-            component={EditProfile}
+            component={SettingUser}
           />
-          <PrivateRoute exact path="/post/edit/:postId" component={EditPost} />
+          <PrivateRoute
+            exact
+            path="/user/edit/bbg/:userId"
+            component={SettingCollection}
+          />
+           <Route path="*" component={NotFound} />
+           
+           />
         </Switch>
       </>
     );
@@ -63,29 +75,3 @@ class MainRouter extends React.Component {
 
 export default MainRouter;
 
-// const MainRouter = () => (
-//     <div>
-//         <NavBar />
-//         <Switch>
-//             <Route exact path="/" component={Home} />
-//             <PrivateRoute exact path="/admin" component={Admin} />
-
-//             <Route exact path="/forgot-password" component={ForgotPassword} />
-//             <Route exact path="/reset-password/:resetPasswordToken" component={ResetPassword} />
-
-//             <PrivateRoute exact path="/post/create" component={NewPost} />
-//             <Route exact path="/post/:postId" component={Post} />
-
-//             <Route exact path="/Users" component={Users} />
-//             <Route exact path="/signup" component={Signup} />
-//             <Route exact path="/signin" component={Signin} />
-
-//             <PrivateRoute exact path="/findpeople" component={FindPeople} />
-//             <PrivateRoute exact path="/user/:userId" component={Profile} />
-//             <PrivateRoute exact path="/user/edit/:userId" component={EditProfile} />
-//             <PrivateRoute exact path="/post/edit/:postId" component={EditPost} />
-//         </Switch>
-//     </div>
-// );
-
-// export default MainRouter;
---
+++ src/components/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class NotFound extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1 className="page-header">Not Found</h1>
+                <div>
+                    <span>Page Not Found </span>
+                </div>
+
+            </div>
+        )
+    }
+}
+export default NotFound;
