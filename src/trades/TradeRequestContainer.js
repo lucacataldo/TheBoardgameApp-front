2428037286df88af319dda628dc9ba511dedd460
Louie Tse <28274608+tselouie@users.noreply.github.com>
2020-10-13 16:58:31 -0400

    using list items + reactstrap/migrateAdd

--- src/trades/TradeRequestContainer.js
+++ src/trades/TradeRequestContainer.js
@@ -6,7 +6,8 @@ import Button from "react-bootstrap/Button"
 import { getUserId } from "../user/apiUser";
 import { getGuruCollection } from "../boardgame/apiBoardgame";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight, faSearch, faExchangeAlt,faMinusCircle } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faArrowRight, faSearch, faExchangeAlt, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
+import { ListGroup, ListGroupItem, FormGroup, Label, Input } from 'reactstrap';
 
 
 
@@ -25,7 +26,9 @@ class TradeRequestContainer extends React.Component {
     searchedUserPrice: 0,
     searchedUserTotalPrice: 0,
     valueMin: 0,
-    valueMax: 9999
+    valueMax: 9999,
+    userTradeList: [],
+    searchedUserTradeList: []
   }
 
 
@@ -55,8 +58,17 @@ class TradeRequestContainer extends React.Component {
     getUserId(user).then(id => {
       getGuruCollection(id).then(bgList => {
         if (bgList !== undefined)
-
-          this.setState({ searchUser: user, searchedUserBoardgames: bgList, isLoading: false, foundUser: true });
+            if(document.getElementById("filterMatching").checked == true){
+              console.log("CHECKED")
+              bgList = bgList.filter(val => !this.state.userBoardgames.includes(val));
+              let userBoardgames = this.state.userBoardgames.filter(val => !bgList.includes(val));
+              this.setState({ searchUser: user, searchedUserBoardgames: bgList,userBoardgames: userBoardgames, isLoading: false, foundUser: true });     
+               
+            }else{
+              console.log("NOT CHECKED");
+              this.setState({ searchUser: user, searchedUserBoardgames: bgList, isLoading: false, foundUser: true });
+            }
+          
       })
     }).catch(err => {
       console.log(err);
@@ -68,98 +80,90 @@ class TradeRequestContainer extends React.Component {
 
 
   handleAddBoardgame(event) {
- 
-    try{
-      var available = document.getElementById("myList");
-    var tradeBox = document.getElementById("tradedToYou");
-    var price = document.getElementById("bgSetPrice");
-    var number = parseFloat(price.value).toFixed(2);
-    var ID = available.options[available.selectedIndex].id;
+    if (event.target.id == "right1") {
+      try {
+        var available = document.getElementById("myList");
+        var price = document.getElementById("bgSetPrice");
+        var number = parseFloat(price.value).toFixed(2);
+        var ID = available.options[available.selectedIndex].id;
 
-      const values = { id: ID, name: available.options[available.selectedIndex].value, price: number }
-      const tradeItem = Object.create(values);
+        const values = { id: ID, name: available.options[available.selectedIndex].value, price: number }
+        const trades = this.state.userTradeList;
+        const tradeItem = Object.create(values);
+        trades.push(tradeItem);
+        available.removeChild(available.options[available.selectedIndex]);
+        let total = parseFloat(this.state.userTotalPrice) + parseFloat(number);
 
-      var element = document.createElement('option');
-      element.setAttribute("id", tradeItem.id);
-      element.appendChild(document.createTextNode(tradeItem.name + " | $" + tradeItem.price));
-      tradeBox.appendChild(element);
-      available.removeChild(available.options[available.selectedIndex]);
-      let total = parseFloat(this.state.userTotalPrice) + parseFloat(number);
+        this.setState({ userTradeList: trades, userTotalPrice: total.toFixed(2) });
+        return true;
 
-      this.setState({ userTotalPrice: total.toFixed(2) });
-      return true;
-    
-    }catch(e){
-      console.log(e);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        var available = document.getElementById("yourList");
+        var price = document.getElementById("bgSetPrice2");
+        var number = parseFloat(price.value).toFixed(2);
+        var ID = available.options[available.selectedIndex].id;
+        const values = { id: ID, name: available.options[available.selectedIndex].value, price: number }
+        const trades = this.state.searchedUserTradeList;
+        const tradeItem = Object.create(values);
+        trades.push(tradeItem);
+        available.removeChild(available.options[available.selectedIndex]);
+        let total = parseFloat(this.state.searchedUserTotalPrice) + parseFloat(number);
+
+        this.setState({ searchedUserTradeList: trades, searchedUserTotalPrice: total.toFixed(2) });
+        return true;
+
+      } catch (e) {
+        console.log(e);
+      }
     }
-    
+
 
 
   }
   handleRemoveBoardgame(event) {
-    try{
-    var available = document.getElementById("myList");
-    var tradeBox = document.getElementById("tradedToYou");
-    var val = tradeBox.options[tradeBox.selectedIndex].value.split("|");
-    var id = tradeBox.options[tradeBox.selectedIndex].id;
-    var element = document.createElement('option');
-    element.setAttribute("id", id);
-    element.appendChild(document.createTextNode(val[0]));
-    available.appendChild(element);
-    tradeBox.removeChild(tradeBox.options[tradeBox.selectedIndex]);
-    let parsedPrice = val[1].split("$");
-    let total = parseFloat(this.state.userTotalPrice) - parseFloat(parsedPrice[1]);
-    this.setState({ userTotalPrice: total.toFixed(2) });
-    return true;
-  }catch(e){
-    console.log(e);
-  }
-  }
+    try {
+      const trades = this.state.userTradeList;
 
-  handleAddUserBoardgame(event) {
-try{
-    var available = document.getElementById("yourList");
-    var tradeBox = document.getElementById("tradedToMe");
-    var price = document.getElementById("bgSetPrice2");
-    var number = parseFloat(price.value).toFixed(2);
-    var val = available.options[available.selectedIndex].value + " | $" + number;
-    var id = available.options[available.selectedIndex].id;
-    var element = document.createElement('option');
-    element.setAttribute("id", id);
-    element.appendChild(document.createTextNode(val));
-    tradeBox.appendChild(element);
-    available.removeChild(available.options[available.selectedIndex]);
-    let total = parseFloat(this.state.searchedUserTotalPrice) + parseFloat(number);
-    this.setState({ searchedUserTotalPrice: total.toFixed(2) });
-//Make object to store item
-    const ti = { name: val, price: number, condition: "Good" }
-    const obj = Object.create(ti);
-    console.log("TI" + obj.name + obj.price + obj.condition);
+      const foundItem = trades.find(item => item.id == event.currentTarget.id);
 
-    return true;
-  }catch(e){
-    console.log(e);
-  }
+      const removeItem = trades.filter(item => item.id !== event.currentTarget.id);
+      var available = document.getElementById("myList");
+      var element = document.createElement('option');
+      element.setAttribute("id", foundItem.id);
+      element.appendChild(document.createTextNode(foundItem.name));
+      available.appendChild(element);
+
+      let parsedPrice = foundItem.price;
+      let total = parseFloat(this.state.userTotalPrice) - parseFloat(parsedPrice);
+      this.setState({ userTradeList: removeItem, userTotalPrice: total.toFixed(2) });
+      return true;
+    } catch (e) {
+      console.log(e);
+    }
   }
   handleRemoveUserBoardgame(event) {
-    try{
-    var available = document.getElementById("yourList");
-    var tradeBox = document.getElementById("tradedToMe");
-    var val = tradeBox.options[tradeBox.selectedIndex].value.split("|");
-    var id = tradeBox.options[tradeBox.selectedIndex].id;
-    var element = document.createElement('option');
-    element.setAttribute("id", id);
-    element.appendChild(document.createTextNode(val[0]));
-    available.appendChild(element);
-    tradeBox.removeChild(tradeBox.options[tradeBox.selectedIndex]);
-    let parsedPrice = val[1].split("$");
-    let total = parseFloat(this.state.searchedUserTotalPrice) - parseFloat(parsedPrice[1]);
-    this.setState({ searchedUserTotalPrice: total.toFixed(2) });
+    try {
+      const trades = this.state.searchedUserTradeList;
+      const foundItem = trades.find(item => item.id == event.currentTarget.id);
+      const removeItem = trades.filter(item => item.id !== event.currentTarget.id);
+      console.log(foundItem);
+      var available = document.getElementById("yourList");
+      var element = document.createElement('option');
+      element.setAttribute("id", foundItem.id);
+      element.appendChild(document.createTextNode(foundItem.name));
+      available.appendChild(element);
 
-    return true;
-  }catch(e){
-    console.log(e);
-  }
+      let parsedPrice = foundItem.price;
+      let total = parseFloat(this.state.searchedUserTotalPrice) - parseFloat(parsedPrice);
+      this.setState({ searchedUserTradeList: removeItem, searchedUserTotalPrice: total.toFixed(2) });
+      return true;
+    } catch (e) {
+      console.log(e);
+    }
   }
 
 
@@ -204,7 +208,17 @@ try{
               <div className=" col-12 form-inline py-2 px-0">
                 <input id='searchbar' className='w-25 h-100 mr-0 rounded form-control' type='text' name='search' placeholder='Search...' />
                 <Button variant="primary" onClick={this.handleSearchButton.bind(this)}><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></Button>
+                &nbsp;<span><FormGroup check>
+                  <Label check>
+                    <Input id="filterMatching" type="checkbox" />
+                    Filter Matching Games
+        </Label>
+                </FormGroup></span>
+
+
+
               </div>
+
             </div>
             {/* START Recipient trade list */}
             {!this.state.foundUser ?
@@ -238,15 +252,15 @@ try{
                   </div>
 
                   <div className="col-1 justify-content-center">
-                    <button className="p-4 mt-4" id="right2" style={{ marginTop: '200px' }} value=">" onClick={this.handleAddUserBoardgame.bind(this)}><FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></button>
-                    <br />
-                    <button className="p-4" type="button" id="left2" value="<" onClick={this.handleRemoveUserBoardgame.bind(this)}><FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon></button>
+                    <button className="p-4 mt-4" id="right2" style={{ marginTop: '200px' }} value=">" onClick={this.handleAddBoardgame.bind(this)}><FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></button>
 
                   </div>
                   <div className="col-5">
                     <label >To Trade:</label>
-                    <select multiple size="8" className="form-control " id="tradedToMe">
-                    </select>
+                    <ListGroup id="tradedToMe">
+                      {this.state.searchedUserTradeList.map(item => <ListGroupItem key={item.id} id={item.id} className="align-middle" onClick={this.handleRemoveUserBoardgame.bind(this)}>{item.name} | ${item.price}
+                        <FontAwesomeIcon className="align-middle" style={{ float: "right" }} color="red" size="lg" icon={faMinusCircle} ></FontAwesomeIcon></ListGroupItem>)}
+                    </ListGroup>
                     <h3>Total Value: ${this.state.searchedUserTotalPrice}</h3>
                   </div>
                 </div>
@@ -280,16 +294,17 @@ try{
                   <div className="justify-content-center text-center px-5">
 
                     <button type="button" className=" p-4 center-block text-center" style={{ marginTop: '200px' }} id="right1" value=">" onClick={this.handleAddBoardgame.bind(this)}><FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></button>
-                    <br />
-                    <button className="p-4 center-block" id="left1" onClick={this.handleRemoveBoardgame.bind(this)}><FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon></button>
+
 
                   </div>
                   <div className="col-md-5 mt-5">
 
                     <div className="form-group mt-6">
                       <label >To Trade:</label>
-                      <select multiple size="8" className="form-control" id="tradedToYou">
-                      </select>
+                      <ListGroup id="tradedToYou">
+                        {this.state.userTradeList.map(item => <ListGroupItem key={item.id} id={item.id} className="align-middle" onClick={this.handleRemoveBoardgame.bind(this)}>{item.name} | ${item.price}
+                          <FontAwesomeIcon className="align-middle" style={{ float: "right" }} color="red" size="lg" icon={faMinusCircle} ></FontAwesomeIcon></ListGroupItem>)}
+                      </ListGroup>
                       <h3>Total Value: ${this.state.userTotalPrice}</h3>
                     </div>
                   </div>
