c4c8ce77cca44e06f39bb6e0558e9bfe706b979e
unknown <zenna.liang@gmail.com>
2020-10-19 13:43:09 -0400

    switch to use react big calendar

--- src/calendar/Calendar.js
+++ src/calendar/Calendar.js
@@ -1,124 +1,33 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import React, { useState, useContext } from "react";
 import AppContext from "../context/App/appContext";
-import {
+import { format, startOfWeek, parse, getDay } from "date-fns";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+
+const localizer = dateFnsLocalizer({
   format,
+  parse,
   startOfWeek,
-  addDays,
-  addMonths,
-  subMonths,
-  startOfMonth,
-  endOfMonth,
-  endOfWeek,
-  isSameMonth,
-  isSameDay,
-} from "date-fns";
+  getDay,
+  locales,
+});
 
-const Calendar = () => {
-  const [currentMonth, setCurrentMonth] = useState(new Date());
-  const [selectedDate, setSelectedDate] = useState(new Date());
-
+const MyCalendar = () => {
   const calContext = useContext(AppContext);
   const { addEvent, events, colors } = calContext;
-
-  const renderHeader = () => {
-    const dateFormat = "MMMM yyyy";
-
-    return (
-      <div className="header row flex-middle">
-        <div className="col col-start">
-          <div className="icon" onClick={prevMonth}>
-            chevron_left
-          </div>
-        </div>
-        <div className="col col-center">
-          <span>{format(currentMonth, dateFormat)}</span>
-        </div>
-        <div className="col col-end" onClick={nextMonth}>
-          <div className="icon">chevron_right</div>
-        </div>
-      </div>
-    );
-  };
-
-  const renderDays = () => {
-    const dateFormat = "EEEE";
-    const days = [];
-    let startDate = startOfWeek(currentMonth);
-    for (let i = 0; i < 7; i++) {
-      days.push(
-        <div className="col col-center" key={i}>
-          {format(addDays(startDate, i), dateFormat)}
-        </div>
-      );
-    }
-
-    return <div className="days row">{days}</div>;
-  };
-
-  const renderCells = () => {
-    const monthStart = startOfMonth(currentMonth);
-    const monthEnd = endOfMonth(monthStart);
-    const startDate = startOfWeek(monthStart);
-    const endDate = endOfWeek(monthEnd);
-
-    const dateFormat = "d";
-    const rows = [];
-
-    let days = [];
-    let day = startDate;
-    let formattedDate = "";
-
-    while (day <= endDate) {
-      for (let i = 0; i < 7; i++) {
-        formattedDate = format(day, dateFormat);
-        const cloneDay = day;
-        days.push(
-          <div
-            className={`col cell ${
-              !isSameMonth(day, monthStart)
-                ? "disabled"
-                : isSameDay(day, selectedDate)
-                ? "selected"
-                : ""
-            }`}
-            key={day}
-            onClick={() => onDateClick(cloneDay)}
-          >
-            <span className="number">{formattedDate}</span>
-            <span className="bg">{formattedDate}</span>
-          </div>
-        );
-        day = addDays(day, 1);
-      }
-      rows.push(
-        <div className="row" key={day}>
-          {days}
-        </div>
-      );
-      days = [];
-    }
-    return <div className="body">{rows}</div>;
-  };
-
-  const onDateClick = (day) => {
-    setSelectedDate(day);
-  };
-
-  const nextMonth = () => {
-    setCurrentMonth(addMonths(currentMonth, 1));
-  };
-
-  const prevMonth = () => {
-    setCurrentMonth(subMonths(currentMonth, 1));
-  };
-
   return (
-    <div className="calendar col-lg-9 col-xl-10">
-      {renderHeader()}
-      {renderDays()}
-      {renderCells()}
+    <div style={{ height: 700 }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   );
 };
-
-export default Calendar;
+export default MyCalendar;
