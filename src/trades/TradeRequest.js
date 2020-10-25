b2f8df9a1d85ac50178ca7755241e88e918833c2
Louie Tse <28274608+tselouie@users.noreply.github.com>
2020-10-25 15:16:14 -0400

    use data from prop instead of static

--- src/trades/TradeRequest.js
+++ src/trades/TradeRequest.js
@@ -12,6 +12,11 @@ class TradeRequest extends React.Component {
         };
       }
     
+      componentWillMount(){
+
+      }
+
+
       componentDidMount() {
         if (
           isAuthenticated()._id !== this.props.userId &&
@@ -21,47 +26,35 @@ class TradeRequest extends React.Component {
         }
       }
     render() {
-        return <div className="card" >
+        return (
+         <div className="card" >
             <div className="card-header">Waiting for Response</div>
         <div className="card-body">
             <div className="row justify-content-center align-self-center mt-1">
+              {this.props.trades.map(trade=>{
+                return (<>
                 <div className="col-md-2">
                 <img className="rounded-circle" src="https://via.placeholder.com/100" alt="displaypic"/>
                 </div>
                 <div className="col-md-6 mt-4">
       
-                    <a className="font-weight-bold h5" href="/">Jane Doe</a>
+                    <a className="font-weight-bold h5" href={'user/' + trade.tradeReceiver._id}>{trade.tradeReceiver.name} </a>
                     <br/>
-                    <small>3 games</small>
+                    <small>{trade.tradeWants.length} games</small>
                        
                 </div>
                 <div className="col-md-4 mt-4">
                     <button type="button" className="btn btn-danger float-right"><FontAwesomeIcon icon={faTimes} /> Cancel</button>
                 <button type="button" className="btn btn-primary float-right mx-2"><FontAwesomeIcon icon={faEye} /> View</button>
                 
-                </div>        
+                </div></>);        
+                })}
             </div>
-
-            <div className="row justify-content-center align-self-center mt-1">
-                <div className="col-md-2">
-                <img className="rounded-circle" src="https://via.placeholder.com/100" alt="displaypic"/>
-                </div>
-                <div className="col-md-6 mt-4">
-                    <a className="font-weight-bold h5" href="/">Joe Dae</a>
-                    <br/>
-                    <small>1 games</small>
-                </div>
-                <div className="col-md-4 mt-4">
-                <button type="button" className="btn btn-danger float-right"><FontAwesomeIcon icon={faTimes} /> Cancel</button>
-                <button type="button" className="btn btn-primary float-right mx-2"><FontAwesomeIcon icon={faEye} /> View</button>
-                </div>        
-            </div>
-            
-
+            <div className="card-footer text-center"><a href="/" className="btn btn-primary">Show More</a></div>
         </div>
- 
-          <div className="card-footer text-center"><a href="/" className="btn btn-primary">Show More</a></div>
-        </div>;
+        </div> )
+                
+                  
     }
   }
   export default TradeRequest;
