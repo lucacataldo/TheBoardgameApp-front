07b25dff48542e2b6452e6dd45782de47b5bfdc5
luca <luca9716@gmail.com>
2020-10-31 17:00:40 -0400

    Calendar Animator Fix

--- src/animator/Animator.js
+++ src/animator/Animator.js
@@ -3,11 +3,14 @@ import gsap from "gsap";
 class Animator {
     animate(){
         let tl = gsap.timeline();
-        tl.set(".animator", { opacity: 0, y: 50 });
+        tl.set(".animator", { 
+            opacity: 0, 
+            css: {marginTop: 50}
+        });
         tl.to(".animator", {
             duration: 0.4,
             opacity: 1,
-            y: 0,
+            css: {marginTop: 0},
             stagger: 0.07
         });
     }
