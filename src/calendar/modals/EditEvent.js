a79505d0448861fb2a247fa87a48f26750a3b0b6
unknown <zenna.liang@gmail.com>
2020-11-08 13:43:38 -0500

    clean up event codes

--- src/calendar/modals/EditEvent.js
+++ src/calendar/modals/EditEvent.js
@@ -1,39 +1,15 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import "react-datepicker/dist/react-datepicker.css";
+
 import EventForm from "./EventForm";
 import { isAuthenticated } from "../../auth";
 import { EventContext } from "../../context/EventContext";
+
 const EditEvent = (props) => {
-  const { selectedEvent } = useContext(EventContext);
-
-  const [event, setEvent] = useState({
-    _id: "",
-    title: "",
-    allDay: false,
-    description: "",
-    startDate: new Date(),
-    endDate: new Date(),
-    owner: isAuthenticated().user._id,
-    bgColor: "eventTag-Blue",
-  });
-
-  useEffect(() => {
-    if (selectedEvent.length !== 0) {
-      setEvent({
-        _id: selectedEvent._id,
-        title: selectedEvent.title,
-        allDay: selectedEvent.allDay,
-        description: selectedEvent.description,
-        startDate: new Date(selectedEvent.startDate),
-        endDate: new Date(selectedEvent.endDate),
-        owner: selectedEvent.owner,
-        bgColor: selectedEvent.bgColor,
-      });
-    }
-  }, [selectedEvent]);
+  const { selectedEvent, setSelectedEvent } = useContext(EventContext);
 
   const reset = () => {
-    setEvent({
+    setSelectedEvent({
       _id: "",
       title: "",
       allDay: false,
@@ -50,12 +26,11 @@ const EditEvent = (props) => {
       <EventForm
         modalId="edit-event"
         modalTitle="Edit Event"
-        eventInfo={{
-          event,
-        }}
+        eventInfo={selectedEvent}
         resetModal={reset}
       />
     </>
   );
 };
+
 export default EditEvent;
