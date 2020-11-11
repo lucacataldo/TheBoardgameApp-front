6bef002a294cca801f8a5735c9ba8a45b421b75e
Louie Tse <28274608+tselouie@users.noreply.github.com>
2020-11-11 15:52:58 -0500

    get events and trades

--- src/components/notifications/Notification.js
+++ src/components/notifications/Notification.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Badge from "@material-ui/core/Badge";
 import IconButton from "@material-ui/core/IconButton";
 import Popover from "@material-ui/core/Popover";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Card from "./Card";
+import { isAuthenticated } from "../../auth";
+import { getEventsByUserId } from "../../calendar/apiCalendar";
+import { getAllTradeRequestsById } from "../../trades/apiTrade";
 
 export default function Notification() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -13,6 +16,21 @@ export default function Notification() {
     { name: "Marley", type: "Request" }
   ]);
 
+  const getNotifications = () => {
+    getEventsByUserId(isAuthenticated().user._id).then(event => {
+      console.log(event);
+    });
+    getAllTradeRequestsById(isAuthenticated().user._id).then(trade => {
+      console.log(trade);
+    });
+  };
+
+  useEffect(() => {
+    return () => {
+      getNotifications();
+    };
+  });
+
   const handleClickOpen = event => {
     setAnchorEl(event.currentTarget);
   };
@@ -20,7 +38,6 @@ export default function Notification() {
   const handleClose = value => {
     setAnchorEl(null);
   };
-  const getNotifications = () => {};
 
   const open = Boolean(anchorEl);
   const id = open ? "simple-popover" : undefined;
