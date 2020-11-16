182058c7061a590b0d01d03d00da23c391792e96
luca <luca9716@gmail.com>
2020-11-16 15:27:25 -0500

    Chat basic implementation no websockets yet frontend just polls server for more messages when chat is open

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -2,7 +2,8 @@ import React from "react"
 import { NavLink, withRouter } from "react-router-dom";
 import { isAuthenticated } from "../auth";
 
-import { getChats, newChat } from "./apiChat";
+import { apiCreateChat, apiGetChat, apiGetChats, apiSendChat } from "./apiChat";
+import moment from "moment";
 
 import "../css/chat.scss"
 
@@ -12,30 +13,32 @@ class Chat extends React.Component {
     super()
     this.state = {
       isOpen: false,
+      chatSelected: false,
+      selectedChat: {},
       user: isAuthenticated().user,
-      chats: []
+      chats: [],
     }
   }
 
   componentDidMount() {
-    this.openChat()
+    this.openChatWindow()
   }
 
-  openChat = async () => {
+  openChatWindow = async () => {
     this.setState({
       isOpen: true
     })
 
     try {
       this.setState({
-        chats: await getChats(isAuthenticated().token)
+        chats: await apiGetChats(isAuthenticated().token)
       })
     } catch (error) {
       console.log(error);
     }
   }
 
-  closeChat = () => {
+  closeChatWindow = () => {
     this.setState({
       isOpen: false
     })
@@ -43,7 +46,7 @@ class Chat extends React.Component {
 
   createChat = async () => {
     try {
-      let resp = await newChat(document.getElementById("usernameSearch").value, isAuthenticated().token)
+      let resp = await apiCreateChat(document.getElementById("usernameSearch").value, isAuthenticated().token)
       let clone = this.state.chats
       clone.push(resp)
       this.setState({
@@ -65,6 +68,88 @@ class Chat extends React.Component {
     }
   }
 
+  chatPoller = 0;
+
+  chatPollerFn = async () => {
+    try {
+      let id = this.state.selectedChat._id
+      if (!id) {
+        clearInterval(this.chatPoller)
+        throw 400
+      }
+      let chat = await apiGetChat(isAuthenticated().token, id)
+      this.setState({
+        selectedChat: chat
+      })
+
+      let list = document.querySelector(".chatList")
+      list.scrollTop = list.scrollHeight;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getChat = async (e) => {
+    try {
+      let id = e.target.dataset.id;
+
+      let chat = await apiGetChat(isAuthenticated().token, id)
+
+
+      this.setState({
+        chatSelected: true,
+        selectedChat: chat
+      })
+      
+      this.chatPoller = setInterval(this.chatPollerFn, 5000)
+
+      let list = document.querySelector(".chatList")
+      list.scrollTop = list.scrollHeight;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  closeChat = () => {
+    this.setState({
+      chatSelected: false,
+      selectedChat: {}
+    })
+  }
+
+  sendChat = async () => {
+    try {
+      let chatId = this.state.selectedChat._id
+      let message = document.getElementById("chatBox").value
+
+      let sent = await apiSendChat(chatId, message, isAuthenticated().token)
+
+      sent.from = isAuthenticated().user
+      console.log("sent\n", sent);
+
+      let clone = this.state.selectedChat;
+      clone.messages.push(sent)
+
+      this.setState({
+        selectedChat: clone
+      })
+
+      console.log(this.state.selectedChat);
+
+      document.getElementById("chatBox").value = ""
+      let list = document.querySelector(".chatList")
+      list.scrollTop = list.scrollHeight;
+    } catch (error) {
+      console.log(error)
+      alert(error)
+    }
+  }
+
+  chatIsFromUser = (id) => {
+    return id === isAuthenticated().user._id
+  }
+
   render() {
     return (
       <div className="chatCont">
@@ -72,38 +157,115 @@ class Chat extends React.Component {
           <div>
             {this.state.isOpen && (
               <div className="chatWindow bg-white p-3 rounded-lg border shadow-sm">
+
                 <div className="d-flex justify-content-between align-items-center">
-                  <span>Chat</span>
-                  <i className="fa fa-angle-down closeChat" onClick={this.closeChat}></i>
+                  {!this.state.chatSelected && (
+                    <div>
+                      <span>Chat</span>
+                    </div>
+                  )}
+                  {this.state.chatSelected && (
+                    <div className="cursor-pointer" onClick={this.closeChat}>
+                      <i className="fa fa-angle-left"></i>
+                    </div>
+                  )}
+                  {this.state.chatSelected && (
+                    <div className="justify-self-center">
+                      {(this.state.selectedChat.between[0]._id !== isAuthenticated().user._id) && (
+                        this.state.selectedChat.between[0].name
+                      )}
+
+                      {(this.state.selectedChat.between[1]._id !== isAuthenticated().user._id) && (
+                        this.state.selectedChat.between[1].name
+                      )}
+                    </div>
+                  )}
+                  <i className="fa fa-angle-down closeChatWindow cursor-pointer" onClick={this.closeChatWindow}></i>
                 </div>
-                <ul className="list-group chatList my-3">
-                  {this.state.chats.map((chat, i) => {
-                    return (
-                      <li className="list-group-item" key={chat._id}>
-                        {(chat.between[0]._id !== isAuthenticated().user._id) && (
-                          chat.between[0].name
-                        )} 
-                        
-                        {(chat.between[1]._id !== isAuthenticated().user._id) && (
-                          chat.between[1].name
-                        )} 
-                      </li>
-                    )
-                  })}
-                </ul>
-                <div className="input-group">
-                  <input id="usernameSearch" type="text" className="form-control border-primary" placeholder="Username to chat with" />
-                  <div className="input-group-append">
-                    <button className="btn btn-outline-primary" onClick={this.createChat}>
-                      <i className="fa fa-plus"></i>
-                    </button>
+
+
+                {!this.state.chatSelected && (
+                  <ul className="list-group chatList my-3">
+                    {this.state.chats.map((chat, i) => {
+                      return (
+                        <li className="list-group-item cursor-pointer" onClick={this.getChat} key={chat._id} data-id={chat._id}>
+                          {(chat.between[0]._id !== isAuthenticated().user._id) && (
+                            chat.between[0].name
+                          )}
+
+                          {(chat.between[1]._id !== isAuthenticated().user._id) && (
+                            chat.between[1].name
+                          )}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                )}
+
+
+                {/* Chat is open */}
+                {this.state.chatSelected && (
+                  <div className="chatList my-3">
+                    {this.state.selectedChat.messages.map((msg, i) => {
+                      return (
+                        <div
+                          key={msg.timestamp}
+                          className={`d-flex justify-content-between rounded-lg chatMsg ${this.chatIsFromUser(msg.from._id) ? 'from' : ''}`}
+                        >
+                          <div>{msg.message}</div>
+                          <div className="msgTime">{moment(msg.timestamp).fromNow()}</div>
+                        </div>
+                      )
+                    })}
                   </div>
-                </div>
+                )}
+
+                {!this.state.chatSelected && (
+                  <div className="input-group">
+                    <input
+                      id="usernameSearch"
+                      type="text"
+                      className="form-control border-primary"
+                      placeholder="Username to chat with"
+                      onKeyUp={(e) => {
+                        if (e.key === 'Enter') {
+                          this.createChat()
+                        }
+                      }}
+                    />
+                    <div className="input-group-append">
+                      <button className="btn btn-primary" onClick={this.createChat}>
+                        <i className="fa fa-plus"></i>
+                      </button>
+                    </div>
+                  </div>
+                )}
+
+                {this.state.chatSelected && (
+                  <div className="input-group">
+                    <input
+                      id="chatBox"
+                      type="text"
+                      className="form-control border-primary"
+                      placeholder="Type your message"
+                      onKeyUp={(e) => {
+                        if (e.key === 'Enter') {
+                          this.sendChat()
+                        }
+                      }}
+                    />
+                    <div className="input-group-append">
+                      <button className="btn btn-primary" onClick={this.sendChat}>
+                        <i className="fa fa-send"></i>
+                      </button>
+                    </div>
+                  </div>
+                )}
               </div>
             )}
 
             {!this.state.isOpen && (
-              <div className="chatOpener bg-white shadow-sm" onClick={this.openChat}>
+              <div className="chatOpener bg-white shadow-sm" onClick={this.openChatWindow}>
                 <i className="fa fa-comments"></i>
               </div>
             )}
--- src/chat/apiChat.js
+++ src/chat/apiChat.js
@@ -1,18 +1,20 @@
-export const getChats = async (token) => {
-    let resp = await fetch(`${process.env.REACT_APP_API_URL}/chat`, {
+let baseUrl = `${process.env.REACT_APP_API_URL}/chat`
+
+export const apiGetChats = async (token) => {
+    let resp = await fetch(`${baseUrl}`, {
         method: "GET",
         headers: {
             Authorization: `Bearer ${token}`
         }
     })
     if (resp.status != 200) {
-        throw "Error"
+        throw resp.status
     }
     return resp.json()
 };
 
-export const newChat = async (who, token) => {
-    return fetch(`${process.env.REACT_APP_API_URL}/chat/start`, {
+export const apiCreateChat = async (who, token) => {
+    return fetch(`${baseUrl}/start`, {
         method: "POST",
         headers: {
             Accept: "application/json",
@@ -28,3 +30,52 @@ export const newChat = async (who, token) => {
             return response.json();
         })
 }
+
+/**
+ * @function apiGetChat
+ * @param {String} token 
+ * @param {String} id 
+ */
+
+export const apiGetChat = async (token, id) => {
+    let resp = await fetch(`${baseUrl}/get/${id}`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    
+    if (resp.status != 200) {
+        throw resp.status
+    }
+
+    return resp.json()
+};
+
+
+/**
+ * 
+ * @param {String} chatId 
+ * @param {Object} message 
+ * @param {String} token Auth token
+ */
+export const apiSendChat = async (chatId, message, token) => {
+    let resp = await fetch(`${baseUrl}/send`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            _id: chatId,
+            message
+        })
+    })
+    
+    if (resp.status != 200) {
+        throw resp.status
+    }
+
+    return resp.json()
+};
